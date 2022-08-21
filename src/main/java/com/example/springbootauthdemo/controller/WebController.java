625e12067ab0358c2b0885517aa312417dd0e949
Ramzi Baaguigui <ramzi.baaguigui1@gmail.com>
2022-08-21 10:25:40 +0100

    Added WebSecurity configuration

--- src/main/java/com/example/springbootauthdemo/controller/WebController.java
+++ src/main/java/com/example/springbootauthdemo/controller/WebController.java
@@ -1,9 +1,25 @@
 package com.example.springbootauthdemo.controller;
 
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
 
+    @GetMapping("/forAll")
+    public ResponseEntity<?> forAll() {
+        return ResponseEntity.ok("this content is available for all");
+    }
 
+    @GetMapping("/authRequired")
+    public ResponseEntity<?> requiresAuth(Authentication auth) {
+        String response =
+                "You need to be authenticated in order to access this content\n" +
+                        "You are currently authenticated as:\n" +
+                        auth;
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/example/springbootauthdemo/security/WebSecurityConfig.java
+++ src/main/java/com/example/springbootauthdemo/security/WebSecurityConfig.java
@@ -31,11 +31,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.addFilterBefore(userAuthFilter, BasicAuthenticationFilter.class);
+        http.authorizeRequests().antMatchers("/register", "/auth", "/forAll").permitAll()
+                .and().authorizeRequests().antMatchers("/logout", "/authRequired").authenticated()
+                .and().cors().and().csrf().disable();
+
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         super.configure(web);
+
     }
 
 }
